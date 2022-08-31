8e4e5f87c707ce04154065f6ec5773449b91d58f
pratheepag <pratheepa.vnr@gmail.com>
2022-08-31 17:22:00 +0530

    Updated with Multiple lock, ThreadPool

---
+++ src/multiplelock/App6.java
@@ -0,0 +1,12 @@
+package multiplelock;
+
+import multiplelock.MultipleLock;
+
+public class App6 {
+    
+    public static void main(String[] args) {
+
+        MultipleLock worker = new MultipleLock();
+        worker.main();
+    }
+}
